8a9014214948bb1ea76ac70db6ca85bb8bd9ffad
tunacim <tunaz783@gmail.com>
2023-03-13 01:00:31 +0300

    created custom hook

---
+++ src/hooks/UseFetch/UseFetch.js
@@ -0,0 +1,26 @@
+const { useEffect,useState } = require("react");
+import axios from "axios";
+
+function UseFetch(url){
+    const[error,setError]=useState(null)
+    const[loading,Setloading]=useState(true)
+    const[data,setData]=useState();
+    const fetchData= async ()=> {
+        try {
+            const {data:responseData}= await axios.get(url)
+            setData(responseData.categories)
+            Setloading(false)
+           
+            
+            
+        } catch (error) {
+            setError(error.message)
+            console.log(error)
+        }
+      
+    }
+    useEffect(()=>{
+        fetchData()
+    },[])
+    return{data,error,loading}
+}export default UseFetch
--- src/pages/Category/Category.js
+++ src/pages/Category/Category.js
@@ -1,34 +1,21 @@
-import axios, { Axios } from "axios";
-import React,{useState,useEffect} from "react";
-import { Text,SafeAreaView ,FlatList} from "react-native";
+
+import React from "react";
+import { Text,SafeAreaView ,FlatList,ActivityIndicator} from "react-native";
 import Config from "react-native-config";
 import CategoryCard from "../../components/CategoryCard"
+import UseFetch from "../../hooks/UseFetch";
 import styles from "./category.style"
 
 function Category(){
     
+    const {loading,data,error}=UseFetch(Config.API_URL)
     
-
-    const[data,setData]=useState();
-
-    useEffect(()=>{ fetchData()},[]);
-
-    
-    
-    const fetchData= async ()=> {
-        try {
-            const {data:CategoryData}= await axios.get(Config.API_URL)
-            setData(CategoryData.categories)
-           
-            
-            
-        } catch (error) {
-            console.log(error)
-        }
-      
-    }
     const renderCategory=({item}) => <CategoryCard category={item}></CategoryCard>
-    
+    if(loading){
+        return <ActivityIndicator size="large"></ActivityIndicator>
+    }
+    if(error){
+        return <Text>{error}</Text>}
     return(
 
         <SafeAreaView style={styles.container}>
